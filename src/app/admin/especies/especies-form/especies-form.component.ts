3ffe4126c42e26b9d18984ddbf284e35d861c075
Ivan Ancallay <ivan.ancallay@gmail.com>
2025-03-25 00:00:33 -0500

    create especies

---
+++ src/app/admin/especies/especies-form/especies-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-especies-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './especies-form.component.html',
+  styleUrl: './especies-form.component.scss'
+})
+export class EspeciesFormComponent {
+
+}
